e960723fedde77bc8df2be5dc3afc7194e4f306a
jgporter24 <110738676+jgporter24@users.noreply.github.com>
2022-11-15 23:35:03 -0600

    cores error

--- server.js
+++ server.js
@@ -11,25 +11,25 @@ app.use(express.static('public'));
 app.use(express.json());
 
 app.post('/api/notes', (req, res) => {
-  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
     var notes = JSON.parse(data);
     let userNote = req.body;
     userNote.id = Math.floor(Math.random() * 5000);
     notes.push(userNote);
-  fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
+  fs.writeFile('db/db.json', JSON.stringify(notes), (err, data) => {
       res.json(userNote);
   });
   }); 
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
     let notes = JSON.parse(data);
     const newNotes = notes.filter(note => note.id !== parseInt(req.params.id));
   
-  fs.writeFile('./db/db.json', JSON.stringify(newNotes), (err, data) => {
+  fs.writeFile('db/db.json', JSON.stringify(newNotes), (err, data) => {
     res.json({msg: 'successfully'});
   });
 });
@@ -40,7 +40,7 @@ app.get('api/notes/:id', (req, res) =>{
 });
 
 app.get('/api/notes', (req, res) => {
-  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
     var notes = JSON.parse(data);
     res.json(notes);
