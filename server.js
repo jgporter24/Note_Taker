6d12086d6acecbfbdb817f75b5426f14bf8305cf
jgporter24 <110738676+jgporter24@users.noreply.github.com>
2022-11-15 23:28:28 -0600

    debugging cors error

--- server.js
+++ server.js
@@ -1,22 +1,60 @@
-//Dependencies
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 
-//set up express app
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 app.use(express.static('public'));
+app.use(express.json());
 
-//routers
+app.post('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    var notes = JSON.parse(data);
+    let userNote = req.body;
+    userNote.id = Math.floor(Math.random() * 5000);
+    notes.push(userNote);
+  fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
+      res.json(userNote);
+  });
+  }); 
+});
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+app.delete('/api/notes/:id', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    let notes = JSON.parse(data);
+    const newNotes = notes.filter(note => note.id !== parseInt(req.params.id));
+  
+  fs.writeFile('./db/db.json', JSON.stringify(newNotes), (err, data) => {
+    res.json({msg: 'successfully'});
+  });
+});
+});
 
-//starts server to begin listening
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+app.get('api/notes/:id', (req, res) =>{
+  res.json(notes[req.params.id]);
+});
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    var notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/notes.html'))
+});
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, '/index.html'));
+});   
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
 });
